9fda611b63735dc61fdef9907f97894fdbdd63e0
Muhammad Akbar Khan <akbarrkhan@outlook.com>
2023-02-24 11:33:26 +0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mac on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
